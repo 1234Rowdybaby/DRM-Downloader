0a2cd897ee6720e435b7e2e71054ae447a7a1d59
1234Rowdybaby <167402968+1234Rowdybaby@users.noreply.github.com>
2025-06-01 21:46:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,13 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from config import Config
 
-from pyrogram import Client
+from pyrogram import Client, filters  # Add filters import (safe if used in plugins)
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(
-        root="plugins"
-    )
+    plugins = dict(root="plugins")
     app = Client(
         "URL Uploader",
         bot_token=Config.TG_BOT_TOKEN,
